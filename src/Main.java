2ca86d2329ae5b22e4a8c0180a294a68a256030d
Сергей Николаевич <tukmachev.18@yandex.ru>
2023-01-20 19:42:51 +0300

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
 public class Main {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        car Car = new car("Lada", "Granta", 1.7f, "Желтый", 2015, "Россия","Механическая", "Седан", "123213", 6);
+        Car.information();
+    }
 }
